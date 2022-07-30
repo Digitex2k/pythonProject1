d1fbd7b1bc4d0c0471db28cd191da4190c87d141
Digitex2k <digitex2k@gmail.com>
2022-07-30 20:56:43 +1000

    Test1

--- age.py
+++ age.py
@@ -1,13 +1,8 @@
 age = input("Введите ваш возраст: ")
 if int(age) >= 18:
     print("Добро пожаловать!")
-#elif type(age) != int:
-   #print("Вы ввели неверное значение!")
 else:
     print("Доступ запрещён.")
-
 name = input("Введите имя: ")
 a1 = "Верно!" if name == "Олег" else "Вы не Олег!"
 print(a1)
-
-
